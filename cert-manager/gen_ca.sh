d96b84287d3191e070f211bb36ddc335071602b2
yangtianfu <yang.tianfu@znwltech.com>
2020-07-24 22:45:30 +0800

    cert-manager

---
+++ cert-manager/gen_ca.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Generate a CA private key
+openssl genrsa -out ca.key 2048
+
+# Create a self signed Certificate, valid for 10yrs with the 'signing' option set
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=DC" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+
+kubectl create secret tls ca-key-pair \
+   --cert=ca.crt \
+   --key=ca.key \
+   --namespace=cert-manager
